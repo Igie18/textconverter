b362af9def04fafffb0e9a199fcae854f004be68
luigi <luigi@viildz.com>
2023-05-28 02:19:05 +0800

    Remove unnecessaries and added xml comments

--- webapi/Hubs/AsyncExtractorHub.cs
+++ webapi/Hubs/AsyncExtractorHub.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System.Runtime.CompilerServices;
 using System.Threading.Channels;
 using System.Text;
 
@@ -7,6 +6,12 @@ namespace webapi.Hubs
 {
     public class AsyncExtractorHub : Hub
     {
+        /// <summary>
+        /// This is the stream that the frontend can access using signalr hubs and this will call the stream writer that will return each character until it is completed or resulted to error.
+        /// </summary>
+        /// <param name="inputTxt">Frontend can pass their input that they want to convert to base64 format</param>
+        /// <param name="cancellationToken">Frontend may dispose their subscription to the stream using this cancellation token</param>
+        /// <returns></returns>
         public ChannelReader<char> Extract(
         string inputTxt,
         CancellationToken cancellationToken)
@@ -16,6 +21,14 @@ namespace webapi.Hubs
             return channel.Reader;
         }
 
+        /// <summary>
+        /// This will encode the inputTxt and will convert into its base64 format.
+        /// Each character of the base64 will be pass to the steam one by one with random delays between 1-5 secs.
+        /// </summary>
+        /// <param name="writer">This will be the medium of the frontend to fetch the result realtime</param>
+        /// <param name="inputTxt">The text that will be converted to base64 format</param>
+        /// <param name="cancellationToken">Access to cancel the process of converting and writing</param>
+        /// <returns></returns>
         private async Task WriteItemsAsync(
             ChannelWriter<char> writer,
             string inputTxt,
@@ -24,13 +37,13 @@ namespace webapi.Hubs
             Exception localException = null;
             try
             {
-                Random rnd = new Random();
-                int delay = (rnd.Next(1, 5)) * 1000;
+                Random rnd = new Random();                
 
                 var output = Convert.ToBase64String(Encoding.UTF8.GetBytes(inputTxt));
 
                 foreach (var c in output)
                 {
+                    int delay = (rnd.Next(1, 5)) * 1000;
                     await writer.WriteAsync(c, cancellationToken);
                     await Task.Delay(delay, cancellationToken);
                 }
--- webapi/Program.cs
+++ webapi/Program.cs
@@ -3,18 +3,12 @@ using webapi.Hubs;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
         builder =>
         {
+            //Allow the cors for the react frontend endpoint
             builder.WithOrigins("https://localhost:3000")
                 .AllowAnyHeader()
                 .WithMethods("GET", "POST")
@@ -24,18 +18,9 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
-app.MapControllers();
 
 app.UseCors();
 
